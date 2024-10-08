000e86175887fe0f8c6bf063c044ee60fa940b1b
ElKidd7 <ramirez.axel34@uabc.edu.mx>
2024-10-08 07:55:43 -0700

    lab poo 2.1.1

---
+++ src/lab/pkg2/pkg1/pkg1/lpoo/Main.java
@@ -0,0 +1,52 @@
+
+package lab.pkg2.pkg1.pkg1.lpoo;
+
+
+public class Main {
+    public static void main(String[] args) {
+    Estudiante est1= new Estudiante();
+    
+    est1.setNombre("Axel Ramirez");
+    est1.setMatricula("1292979");
+    est1.setFacultad("FCQI");
+    est1.setSemestre(6);
+    
+    System.out.println("Nombre: " + est1.getNombre());
+    System.out.println("Matricula: " + est1.getMatricula());
+    System.out.println("Facultad: " + est1.getFacultad());
+    System.out.println("Semestre: " + est1.getSemestre());
+    
+    System.out.println("------------------------------------");
+    
+    
+    Estudiante est2= new Estudiante();
+    
+    est2.setNombre("Candy Rose");
+    est2.setMatricula("1284075");
+    est2.setFacultad("FCQI");   
+    est2.setSemestre(6);
+    
+    System.out.println("Nombre: " + est2.getNombre());
+    System.out.println("Matricula: " + est2.getMatricula());
+    System.out.println("Facultad: " + est2.getFacultad());
+    System.out.println("Semestre: " + est2.getSemestre());
+    
+    System.out.println("------------------------------------");
+    
+    Estudiante est3= new Estudiante();
+    
+    est3.setNombre("Javi Meraz");
+    est3.setMatricula("1298012");
+    est3.setFacultad("FCQI");
+    est3.setSemestre(6);
+    
+    System.out.println("Nombre: " + est3.getNombre());
+    System.out.println("Matricula: " + est3.getMatricula());
+    System.out.println("Facultad: " + est3.getFacultad());
+    System.out.println("Semestre: " + est3.getSemestre());
+    
+    
+    
+    }
+    
+}
